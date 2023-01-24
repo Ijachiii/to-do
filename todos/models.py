2a6c926d8241c274cbf0dee2cb7b0bbaadd376af
David Audu <davidaudu1010@gmail.com>
2023-01-24 01:59:25 +0100

    models migrations

--- todos/models.py
+++ todos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=200)
-    ody = models.TextField()
+    body = models.TextField()
 
     def __str__(self):
         return self.title
